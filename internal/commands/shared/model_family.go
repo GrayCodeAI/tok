3431f2842162008e9cb72ec38ab16e246c260d09
Patel230 <Lakshmanp230@gmail.com>
2026-04-12 00:20:13 +0530

    chore: remove dead code from commands/shared
    Remove redundant model_family.go wrapper:
    - File just delegated to utils.GetModelFamily
    - Tests updated to remove TestGetModelFamily
    - Use utils.GetModelFamily directly instead
    
    Also removed dotnet_trx.go (401 lines):
    - File was not imported anywhere
    - All functions were unreachable
    - ParseTRXFile, ParseTRXContent, ParseBuildOutput, etc.
    
    Total: ~450 lines of dead code removed
    
    Verification:
    - go build ./...: SUCCESS
    - go test ./...: ALL PASS

--- internal/commands/shared/model_family.go
+++
@@ -1,9 +0,0 @@
-package shared
-
-import "github.com/GrayCodeAI/tokman/internal/utils"
-
-// GetModelFamily extracts the model family from a model name.
-// Delegates to utils.GetModelFamily for single source of truth.
-func GetModelFamily(modelName string) string {
-	return utils.GetModelFamily(modelName)
-}
